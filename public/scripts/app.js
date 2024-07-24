3e215d33951704f2beef6b0c59bf5da0c6435453
massomeh <ms.mohammadi69@gmail.com>
2024-07-24 16:21:35 +0330

    new10 commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,21 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
-        document.documentElement.classList.remove("dark");
-        localStorage.theme = "light";
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
-    }
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+const submenuOpenBtn = document.querySelector(".submenu-open-btn");
+const submenu =document.querySelector(".submenu");
+
+toggleThemeBtns.forEach(btn => {
+    btn.addEventListener("click" , function() {
+        if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        }     
+    })
 })
+
+
+submenuOpenBtn.addEventListener("click", (e) => {
+    console.log(e.currentTarget);
+    submenu.classList.toggle("submenu--open");
+});
